fa1eedb98530d49d9eb9f89088506fe2d2a6206a
geethika-avuthu <108684753+geethika-avuthu@users.noreply.github.com>
2022-07-13 10:07:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+let userNameEl = document.getElementById('userName');
+let email = document.getElementById('inputEmail');
+let panNumber = document.getElementById('pannumber');
+let loanAmount = document.getElementById('inputLoanAmount');
+let firstNumberElement = document.getElementById('firstNumber');
+let secondNumberElement = document.getElementById('secondNumber');
+let userInputElement = document.getElementById('userInput');
+let captchaResultElement = document.getElementById('captchaResult');
+
+
+function allLetter(userNameEl, mx) {
+    var letters = /^[A-Za-z]+$/;
+    var userNameEl_len = userNameEl.value.length;
+    if (userNameEl_len == 0 || userNameEl_len < mx) {
+        alert(
+            'Username must have alphabets and spaces only / length should be minimum ' +
+            mx
+        );
+        userNameEl.focus();
+        return false;
+    } else if (userNameEl.value.match(letters) || userNameEl.value.match(' ')) {
+        return true;
+    }
+}
+
+function ValidateEmail(email) {
+    var mailformat = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+
+    if (email.value.match(mailformat)) {
+        return true;
+    } else if (email.value === '') {
+        alert('enter email');
+    } else {
+        alert('You have entered an invalid email address!');
+        email.focus();
+        return false;
+    }
+}
+
+function alphanumeric(panNumber, mx) {
+    var letters = /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/;
+    var panNumber_len = panNumber.value.length;
+    if (
+        panNumber.value.match(' ') ||
+        panNumber_len == 0 ||
+        panNumber_len < mx ||
+        panNumber_len > mx
+    ) {
+        alert('PAN Number must have alphanumeric characters only');
+        panNumber.focus();
+        return false;
+    } else if (panNumber.value.match(letters)) {
+        return true;
+    }
+}
+
+function allnumeric(loanAmount, mx) {
+    var numbers = /^[0-9]+$/;
+    var loanAmount_len = loanAmount.value.length;
+    if (
+        loanAmount.value.match(' ') ||
+        loanAmount_len <= 0 ||
+        loanAmount_len > mx
+    ) {
+        alert('Loan amount must have numeric and 9 digits only');
+        loanAmount.focus();
+        return false;
+    } else if (loanAmount.value.match(numbers)) {
+        return true;
+    }
+}
+
+function refreshCaptcha() {
+    let firstRandomNumber = Math.random() * 100;
+    let secondRandomNumber = Math.random() * 100;
+    firstNumberElement.textContent = Math.ceil(firstRandomNumber);
+    secondNumberElement.textContent = Math.ceil(secondRandomNumber);
+    captchaResultElement.textContent = '';
+    userInputElement.value = '';
+}
+refreshCaptcha();
+
+function submitBtn() {
+    if (allLetter(userNameEl, 4)) {
+        if (ValidateEmail(email)) {
+            if (alphanumeric(panNumber, 10)) {
+                if (allnumeric(loanAmount, 9)) {
+
+                }
+            }
+        }
+    }
+    return false;
+}
+
+// document.getElementById('form').addEventListener('submit', submitBtn);
---
+++ thank_you.js
@@ -0,0 +1,27 @@
+let Result = document.getElementById("Result");
+let userInput = document.getElementById("userInput");
+let randomNumber = Math.ceil((Math.random() * 10000), 5);
+console.log(randomNumber);
+
+window.addEventListener('load', () => {
+    // Via local Storage
+    /* const name = localStorage.getItem('NAME');
+    const surname = localStorage.getItem('EMAIL'); */
+
+    const name = sessionStorage.getItem('NAME');
+    const surname = sessionStorage.getItem('EMAIL');
+
+    document.getElementById('inputName').innerHTML = userNameE1;
+    document.getElementById('inputEmail').innerHTML = emailE1;
+});
+function checkGuess() {
+    let guessedNumber = parseInt(userInput.value)
+    if (guessedNumber > randomNumber || guessedNumber < randomNumber) {
+        Result.textContent = "Wrong OTP!";
+    } else if (guessedNumber !== randomNumber) {
+        Result.textContent = "Please provide a valid OTP";
+    } else if (guessedNumber === randomNumber) {
+        Result.textContent = "Correct OTP";
+    }
+
+}
