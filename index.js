bf1a08a49378268063947391b16996232c393c50
geethika-avuthu <108684753+geethika-avuthu@users.noreply.github.com>
2022-07-13 10:11:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,9 +23,8 @@ function ValidateEmail(email) {
     var mailformat = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
     if (email.value.match(mailformat)) {
         return true;
-    } else if (email.value === '') {
-        alert('enter email');
-    } else {
+    } 
+    else {
         email.focus();
         return false;
     }
